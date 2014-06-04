6d8542884747b3523918f5cbe3475d3a9ec01b3e
Aladine <alazaiane@yahoo.fr>
2014-06-04 23:01:00 +0200

    animation

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -2,7 +2,7 @@ $('document').ready(function() {
 
 	$(".btn-start").click(function() {
 		$( ".btn-start" ).hide( "slow" );
-		$( "#ibn-saoud, #faycal, #khaled, #fahd, #abdallah, #mouqrin, #salman" ).slideUp( 300 ).delay( 800 ).fadeIn( 400 );
+		$( "#ibn-saoud, #faycal, #khaled, #fahd, #abdallah, #mouqrin, #salman, .btn-wahab" ).slideUp( 300 ).delay( 800 ).fadeIn( 400 );
 	});
 
 	$("#ibn-saoud").click(function() {
@@ -32,4 +32,25 @@ $('document').ready(function() {
 	$("#salman").click(function() {
 		$(".salman-bis").slideUp( 300 ).delay( 800 ).fadeIn( 400 );
 	});
+
+	$ (".btn-wahab").click(function() {
+		$("#ibn-saoud, #faycal, #khaled, #fahd, #abdallah, #mouqrin, #salman, .is-bis, .khaled-bis, .faycal-bis, .fahd-bis, .mouqrin-bis, .salman-bis, .abdallah-bis").hide("slow");
+		$(".media").slideUp( 300 ).delay( 800 ).fadeIn( 400 );
+
+	});
+
+	function doIt() {
+		$( ".quest1, .rep1" ).show( "slow" );
+	}
+// Can pass in function name
+$( ".start1" ).click( doIt );
+
+$( "form" ).submit(function( event ) {
+	if ( $( "input" ).val() === "yes" ) {
+		$( "#rep1" ).show( 4000, function() {
+			$( this ).text( "Ok, DONE! (now showing)" );
+		});
+	}
+	$( ".quest1, .rep1" ).hide( "fast" );
+});
 });

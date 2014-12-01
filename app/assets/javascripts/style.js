05441ae1354bbc47b9a1bcc2c63840818c427413
Aladine <alazaiane@yahoo.fr>
2014-12-01 11:32:45 +0100

    js bg

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -168,7 +168,7 @@ $('document').ready(function() {
 
 	$(".suivant-end").click(function() {
 		$( ".centrafrique" ).hide( "slow" );
-		$( ".end, .pagination" ).slideUp( 300 ).delay( 800 ).fadeIn( 400 );
+		$( "background").css(background-image, url("BG_QUIZZ.jpg");
 	});
 
 

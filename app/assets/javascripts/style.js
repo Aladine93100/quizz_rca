0d348ec876c51369724071b2b6d12ae2dd3eff9c
Aladine <alazaiane@yahoo.fr>
2014-12-15 18:44:02 +0100

    css .end et bouton

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -247,7 +247,7 @@ $('document').ready(function() {
 
 
 	$("#q1").click(function() {
-		$( ".end, .question1, .question2, .question2-bis, .question2-ter, .question3, .question4" ).hide( "slow" );
+		$( ".end, .question1, .question2, .question2-bis, .question2-ter, .question3, .question4, .vrai1, .faux1" ).hide( "slow" );
 		$( ".question5" ).slideUp( 300 ).delay( 800 ).fadeIn( 400 );
 	});
 
